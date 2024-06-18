e24f1b2c05b0d8bc75e230a7a90767056322cf91
Koushik <koushik.bag@wseinfratech.com>
2024-06-18 19:26:17 +0530

    comment 1

---
+++ eatWell/src/test/java/com/example/eatWell/EatWellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eatWell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatWellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
