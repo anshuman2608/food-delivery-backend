b56be20581465944c0cc5dd06955e205c2ff1e22
Anshuman <asingh851127@gmail.com>
2024-07-05 15:56:49 +0530

    security-- branch

---
+++ eatWell/src/main/java/com/example/eatWell/exception/SomeSecurityException.java
@@ -0,0 +1,8 @@
+package com.example.eatWell.exception;
+
+public class SomeSecurityException extends Exception{
+
+    public SomeSecurityException(){
+        super("Some security exception occured");
+    }
+}
